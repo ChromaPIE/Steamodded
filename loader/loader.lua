93397b6e3e83b4d5758ac100e32a6c075a70da02
ChromaPIE <chromasetha@outlook.com>
2024-07-30 14:15:24 +0800

    boot timer for mobile

--- loader/loader.lua
+++ loader/loader.lua
@@ -362,20 +362,24 @@ end
 function boot_timer(_label, _next, progress)
     progress = progress or 0
     G.LOADING = G.LOADING or {
-        font = love.graphics.setNewFont("resources/fonts/m6x11plus.ttf", 20),
+        font = love.graphics.setNewFont("resources/fonts/NotoSansSC-Bold.ttf", 15),
         love.graphics.dis
     }
-    local realw, realh = love.window.getMode()
+    local realw, realh = love.graphics.getDimensions() -- getMode()
     love.graphics.setCanvas()
     love.graphics.push()
     love.graphics.setShader()
     love.graphics.clear(0, 0, 0, 1)
-    love.graphics.setColor(0.6, 0.8, 0.9, 1)
-    if progress > 0 then love.graphics.rectangle('fill', realw / 2 - 150, realh / 2 - 15, progress * 300, 30, 5) end
+    love.graphics.setColor(1, 0, 88/255, 1) -- setColor(0.6, 0.8, 0.9, 1)
+    if progress > 0 then love.graphics.rectangle('fill', realw / 2 - 125, realh / 2 - 60, progress * 300, 30, 5) end
+ -- if progress > 0 then love.graphics.rectangle('fill', realw / 2 - 150, realh / 2 - 15, progress * 300, 30, 5) end
     love.graphics.setColor(1, 1, 1, 1)
     love.graphics.setLineWidth(3)
-    love.graphics.rectangle('line', realw / 2 - 150, realh / 2 - 15, 300, 30, 5)
-    love.graphics.print("LOADING: " .. _next, realw / 2 - 150, realh / 2 + 40)
+    love.graphics.rectangle('line', realw / 2 - 125, realh / 2 - 60, 300, 30, 5)
+ -- love.graphics.rectangle('line', realw / 2 - 150, realh / 2 - 15, 300, 30, 5)
+    love.graphics.print("Balachrom 启动中 ……", realw / 2 - 200, realh / 2 - 5)
+    love.graphics.print("正在加载：" .. _next, realw / 2 - 200, realh / 2 + 25)
+ -- love.graphics.print("LOADING: " .. _next, realw / 2 - 150, realh / 2 + 40)
     love.graphics.pop()
     love.graphics.present()
 
